6af62ffaf23e98416244815d505dfa271f87e55e
Alexandra Campo <alexandracampo86@gmail.com>
2023-02-14 10:49:39 +0100

    created game

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const gameNumber = document.querySelector('.js-number');
+const btnGame = document.querySelector('.js-button');
+const clue = document.querySelector('.js-clue');
+const counter = document.querySelector('.js-counter');
+
+let contador = 0;
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+};
+
+const randomNumber = getRandomNumber(100)
+console.log(randomNumber);
+
+function randomGame(event) {
+
+    const numberChoice = gameNumber.value;
+    console.log(numberChoice);
+
+    if (numberChoice > 100 || numberChoice < 1) {
+        clue.innerHTML = "El número debe estar entre 1 y 100"
+    } else if (numberChoice > randomNumber) {
+        clue.innerHTML = "Demasiado alto";
+    } else if (numberChoice < randomNumber) {
+        clue.innerHTML = "Demasiado bajo"
+    } else if (numberChoice === randomNumber) {
+        clue.innerHTML = '¡Has ganado, campeona!'
+    } else {
+        clue.innerHTML = "El número debe estar entre 1 y 100"
+    }
+
+    contador++;
+    counter.innerHTML = `Número de intentos: ${contador}`;
+
+};
+
+btnGame.addEventListener('click', randomGame);
+
+
+
+
+
+
